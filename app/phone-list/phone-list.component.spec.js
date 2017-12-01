f108c5233c9074c191ade81d8232336fb837bf3f
bruno78 <devbruno78@gmail.com>
2017-12-01 10:10:56 -0500

    add sort phone list by name and age and unit tests

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -4,15 +4,22 @@ describe('phoneList', function() {
 
   // Load the module that contains the `phonelist` component before each test.
   beforeEach(module('phoneList'));
+  
   // Test the controller
   describe('PhoneListController', function() {
+    var ctrl;
 
-    it('should create a `phones` model  with 3 phones',
-      inject(function($componentController) {
-        var ctrl = $componentController('phoneList');
+    beforeEach(inject(function($componentController) {
+      ctrl = $componentController('phoneList');
+    }));
 
-        expect(ctrl.phones.length).toBe(3);
-      }));
+    it('should create a `phones` model with 3 phones', function() {
+      expect(ctrl.phones.length).toBe(3);
+    });
+
+    it('should set a default value for the `orderProp` model', function() {
+      expect(ctrl.orderProp).toBe('age');
+    });
 
   });
   
